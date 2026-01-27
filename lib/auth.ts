1a6c64721051aa19aef148ebd4b8eff00f084da6
unknown <dagimt369@gmail.com>
2026-01-27 12:43:02 +0200

    auth fixed

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,6 +3,7 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 import { PrismaClient } from "@prisma/client";
 import { nextCookies } from "better-auth/next-js";
 
+
 const prisma = new PrismaClient();
 export const auth = betterAuth({
     database: prismaAdapter(prisma, { provider: "postgresql" }),

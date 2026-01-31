6c551f30a56e9a4166dae7e9350043dd773a8e46
unknown <dagimt369@gmail.com>
2026-01-31 11:58:28 +0200

    prisma linux config

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,11 @@
-import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin'
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.plugins = [...config.plugins, new PrismaPlugin()]
-    }
-    return config
+  typescript: {
+    ignoreBuildErrors: true,
   },
-};
+  images: {
+    unoptimized: true,
+  },
+}
 
-export default nextConfig;
+export default nextConfig
